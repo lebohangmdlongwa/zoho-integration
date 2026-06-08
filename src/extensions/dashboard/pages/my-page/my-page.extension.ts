6dcf9db1c172234b0ef05606c94230d430d533ff
lebomdlongwa <lebomdlongwa@gmail.com>
2026-06-08 17:36:07 +0200

    Initial commit from Create Wix App

---
+++ src/extensions/dashboard/pages/my-page/my-page.extension.ts
@@ -0,0 +1,8 @@
+import { extensions } from '@wix/astro/builders';
+
+export default extensions.dashboardPage({
+  id: '1742b6a3-e4ad-4381-a2bc-961133180800',
+  title: 'My Page',
+  routePath: '',
+  component: './extensions/dashboard/pages/my-page/my-page.tsx',
+});
